e32a08d9296178df2217a51ac371602f478e36a5
Akshay Iyer <akshay.iyer@nyu.edu>
2016-06-10 13:53:22 -0400

    Click Counter First Commit

---
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by Akshay Iyer on 6/10/16.
+//  Copyright © 2016 akshaytiyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
